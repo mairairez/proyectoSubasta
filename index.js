e1b38a7ffe6448b07d766f4f68c1ecf16ef797d8
Maira <mairaramirez9898@gmail.com>
2023-10-23 13:16:32 -0300

    creando js

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded",()=>{
+const enviar = document.getElementById("enviar");
+
+enviar.addEventListener('click',()=>{
+    alert("Mensaje enviado satisfactoriamente");
+});
+})
